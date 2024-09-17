cf8cf49cacfa032993fcbb24f07579e06021e9df
Nick Morrison <nick@sensible.so>
2024-09-17 17:28:05 -0400

    add recursive assertions on the library output in tests

--- .github/generator/src/helpers.test.ts
+++ .github/generator/src/helpers.test.ts
@@ -1,5 +1,6 @@
+import assert from "assert";
 import { createTemplateLibrary } from "./helpers";
-import { Library } from "./types";
+import { Library, LibraryGroup } from "./types";
 
 describe("createTemplateLibrary", () => {
   let templateLibrary: string;
@@ -12,7 +13,7 @@ describe("createTemplateLibrary", () => {
     library = parsedLibrary.library;
   });
 
-  it("should create the library", () => {
+  it("should create the library with a nested structure", () => {
     expect(parsedLibrary).toHaveProperty("baseUrl");
 
     expect(library).toHaveProperty("Financial Services");
@@ -23,5 +24,33 @@ describe("createTemplateLibrary", () => {
     expect(library).toHaveProperty("Logistics");
     expect(library).toHaveProperty("Real Estate");
     expect(library).toHaveProperty("Tax Forms");
+
+    recursiveChildrenAssertions(library);
   });
 });
+
+function recursiveChildrenAssertions(children: LibraryGroup["children"]): void {
+  const childKeys = Object.keys(children);
+  for (const key of childKeys) {
+    const child = children[key];
+    expect(child).not.toBeUndefined();
+    expect(child).toHaveProperty("thumbnails");
+    expect(child?.thumbnails.length).toBeGreaterThanOrEqual(1);
+    assert(child);
+
+    if ("schema" in child) {
+      expect(child).toHaveProperty("templates");
+      expect(child.templates.length).toBeGreaterThanOrEqual(1);
+      child.templates.forEach((template) => {
+        expect(template).toHaveProperty("name");
+        expect(template.name.length).toBeGreaterThanOrEqual(1);
+        expect(template).toHaveProperty("refDocs");
+        expect(template.refDocs).toBeInstanceOf(Array);
+        expect(template.refDocs.length).toBeGreaterThanOrEqual(1);
+      });
+    } else if ("children" in child) {
+      expect(Object.keys(child.children).length).toBeGreaterThanOrEqual(1);
+      recursiveChildrenAssertions(child.children);
+    }
+  }
+}
